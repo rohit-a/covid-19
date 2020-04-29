4e578f4e7fc0f641608b665aa92e9b108f0fc363
Rohit Anurag <rohitanurag@outlook.com>
2020-04-29 21:20:25 +0530

    Added Cleanup Script to handle special characters

--- update_india_data.py
+++ update_india_data.py
@@ -19,6 +19,13 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 state_data_list = []
 date = str(datetime.date.today())
+cleanup_list = ['#',","]
+
+
+def clean_value(str_to_clean, cleanup_list=cleanup_list):
+    for ch in cleanup_list:
+        str_to_clean = str_to_clean.replace(ch,'')
+    return str_to_clean
 
 #Extracting Date and overall India data
 india_section = soup.find('div', class_='site-stats-count')
@@ -42,11 +49,11 @@ row_processed, row_skipped = 0, 0
 for row in state_table_rows:
     cols = row.find_all('td')
     if(len(cols) == 5):    
-        s_no = cols[0].text
-        state = cols[1].text
-        confirmed = cols[2].text
-        recovered = cols[3].text
-        deaths = cols[4].text
+        s_no = clean_value(cols[0].text)
+        state = clean_value(cols[1].text)
+        confirmed = clean_value(cols[2].text)
+        recovered = clean_value(cols[3].text)
+        deaths = clean_value(cols[4].text)
         state_data_list.append([s_no, date, country, state, confirmed, recovered, deaths, as_of_date])
         row_processed = row_processed+1
         print("{} {} Extracting {} data".format(row_processed, s_no, state))
@@ -55,13 +62,14 @@ for row in state_table_rows:
         print("{} Skipping {}".format(row_skipped, row.text))
 print("Extracted {} rows data. Skpped {} rows".format(row_processed, row_skipped))
 
+
 #Converting to data frame
 state_df_update = pd.DataFrame(state_data_list, columns=['S_no','Date', 'Country','State','Confirmed','Recovered','Deaths','As_Of_Date'])
-
+ 
 #Reading existing file, filtering out duplicate data for today, appending update and writing back.
 state_df = pd.read_csv("India Covid19 combined data(mohfw snapshot).csv", header=0, names=['S_no','Date', 'Country','State','Confirmed','Recovered','Deaths','As_Of_Date'])
 state_df = state_df[state_df['Date'] != date]
 state_df = state_df.append(state_df_update, ignore_index=True)
-
+ 
 print("Updating CSV for: "+ as_of_date)
 state_df.to_csv("India Covid19 combined data(mohfw snapshot).csv")
