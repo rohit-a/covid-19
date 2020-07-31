fc53b9e46626d833800650472a933b747b6cc766
Rohit Anurag <rohitanurag@outlook.com>
2020-07-31 15:52:00 +0530

    Added Check for Total Cases

--- update_india_data_v2.py
+++ update_india_data_v2.py
@@ -33,6 +33,11 @@ state_df_update['state_name'].replace('Telengana','Telangana',inplace=True)
 
 state_df_update.rename({'sno':'S_no', 'state_name':'State','new_positive':'Confirmed','new_cured':'Recovered','new_death':'Deaths'}, inplace=True, axis=1)
 
+#Check mismatch
+total_mismatch = state_df_update['Confirmed'] != (state_df_update['new_active'] + state_df_update['Recovered'] + state_df_update['Deaths'])
+print("Mismatch States:"+str(state_df_update[total_mismatch]['State'].values))
+state_df_update.loc[total_mismatch,'Confirmed'] = state_df_update['new_active'] + state_df_update['Recovered'] + state_df_update['Deaths']
+
 #Reading existing file, filtering out duplicate data for today, appending update and writing back.
 state_df = pd.read_csv("India Covid19 combined data(mohfw snapshot).csv", header=0, names=['S_no','Date', 'Country','State','Confirmed','Recovered','Deaths','As_Of_Date'])
 state_df = state_df[state_df['Date'] != date]
